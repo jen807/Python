4ae75e51700a3bb16b805df0cedff511d8340911
jen807 <80chill@naver.com>
2025-02-10 12:23:28 +0900

    homework done

--- 2일차/중첩반복문.py
+++ 2일차/중첩반복문.py
@@ -12,11 +12,34 @@
 #     for n in range(1,10):
 #         print(f"{i}x{n}={i*n}")
 
-value = int(input("숫자를 입력해주세요"))
+# value = int(input("숫자를 입력해주세요"))
 
-for n in range(1, 10):
-    print(f"{value} x {n} = {value * n}")
-print(value)
+# for n in range(1, 10):
+#     print(f"{value} x {n} = {value * n}")
+# print(value)
 
 # 1. 두개의 숫자를 입력해서 누가 더 큰지?
+
+value1 = input("첫번째 숫자를 입력해주세요")
+value2 = input("두번째 숫자를 입력해주세요")
+
+if value1 > value2:
+    print(f"{value1}(이)가 {value2}보다 더 큽니다")
+elif value2 > value1:
+    print(f"{value2}(이)가 {value1}보다 더 큽니다")
+else:
+    print(f"두 숫자가 같습니다")
+
+
 # 2. 변수로 임의의 유저 아이디와 패스워드를 저장하고 input을 통하여 입력한 아이디와 패스워드가 맞는지 확인할 수 있는 유효성 로직 구현
+
+id = "30"
+pw = "30"
+
+value1 = input("아이디를 입력해주세요")
+value2 = input("비밀번호를 입력해주세요")
+
+if value1 == id and value2 == pw:
+    print(f"{id}님 로그인 되셨습니다")
+else:
+    print(f"비밀번호나 아이디가 틀렸습니다. 다시 입력해주십시오")
