18766014437829d26d7baef70683a11a1fbe664e
jen807 <80chill@naver.com>
2025-02-12 17:49:00 +0900

    과제

--- 3일차/리턴.py
+++ 3일차/리턴.py
@@ -1,10 +1,10 @@
-def plus(a, b):
-   return a + b
+# def plus(a, b):
+#    return a + b
 
-result = plus(10,20)
+# result = plus(10,20)
 
-print(plus(10,20))
-print(result)
+# print(plus(10,20))
+# print(result)
 
 # *문제
 
@@ -16,3 +16,20 @@ print(result)
 
 # 어린이를 매개변수로 입력시 출력결과 어린이 입니다, 500원 차감 되었습니다.
 # 잔액은 ~입니다.
+
+def card(type, left):
+   card_money = {
+      "어린이": 500,
+      "일반": 1300
+   }
+   if type in card_money:
+      left = left - card_money[type]
+      print(f"{type}입니다, 잔액은 {left}원 입니다.")
+   else:
+      print("카드를 다시 한번 대주십시오")
+
+   return left
+
+card("어린이",10000)
+card("일반",10000)
+card("엥",10000)
