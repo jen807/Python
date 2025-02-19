7ae104c88c2de178ae463c1ff0fdccf704d2225c
jen807 <80chill@naver.com>
2025-02-19 13:52:47 +0900

    done

--- 7일차/예외2.py
+++ 7일차/예외2.py
@@ -1,9 +1,18 @@
-try:
-    # print(10/0)
-    num[0]
-except Exception as e:
-    print("🚨")
-    print(e)
-    print(type(e))
+# try:
+#     # print(10/0)
+#     num[0]
+# except Exception as e:
+#     print("🚨")
+#     print(e)
+#     print(type(e))
 
 # print(10 / 0)
+
+# front end
+# => UI, UX
+# =>html, css, js
+
+# Backend
+# =>server
+# =>DataBase
+# =>python, java, c, php, node js.. etc
